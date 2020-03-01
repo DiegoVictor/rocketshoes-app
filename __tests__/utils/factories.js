cef8a0fcaf4b946f4e89f7e42596a42b9310d38c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-01 00:01:19 -0300

    Use literal objects

--- __tests__/utils/factories.js
+++ __tests__/utils/factories.js
@@ -1,20 +1,25 @@
 import faker from 'faker';
 import { factory } from 'factory-girl';
 
-class Product {}
-class Stock {}
+factory.define(
+  'Product',
+  {},
+  {
+    id: faker.random.number,
+    title: faker.name.title,
+    image: faker.image.imageUrl,
+    amount: () => faker.random.number({ min: 2, max: 5 }),
+    price: faker.random.number,
+  }
+);
 
-factory.define('Product', Product, {
-  id: faker.random.number,
-  title: faker.name.title,
-  image: faker.image.imageUrl,
-  amount: () => faker.random.number({ min: 2, max: 5 }),
-  price: faker.random.number,
-});
-
-factory.define('Stock', Stock, {
-  id: faker.random.number,
-  amount: () => faker.random.number({ min: 2, max: 5 }),
-});
+factory.define(
+  'Stock',
+  {},
+  {
+    id: faker.random.number,
+    amount: () => faker.random.number({ min: 2, max: 5 }),
+  }
+);
 
 export default factory;
