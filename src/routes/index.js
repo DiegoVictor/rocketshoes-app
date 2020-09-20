056746c8158506ba45e67ae7be713ac3adaf6739
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-20 10:40:03 -0300

    Update routes

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Header from '~/components/Header';
+import navigation from '~/services/navigation';
+import Home from '~/pages/Home';
+import Cart from '~/pages/Cart';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default () => (
+  <NavigationContainer ref={navigation}>
+    <Navigator
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: '#191920',
+        },
+        header: () => <Header />,
+      }}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="Cart" component={Cart} />
+    </Navigator>
+  </NavigationContainer>
+);
