32284e4361acfa6c7efe50466957700057184b85
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-20 10:37:19 -0300

    Remove reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,16 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { all } from 'redux-saga/effects';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Reactotron from 'reactotron-react-native';
 
 import cart from './reducers/cart';
 import cartSaga from './sagas/cart';
 
-const sagaMiddleware = createSagaMiddleware({
-  sagaMonitor: (() => {
-    if (__DEV__) {
-      return Reactotron.createSagaMonitor();
-    }
-    return null;
-  })(),
-});
-
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   combineReducers({
     cart,
   }),
-  (() => {
-    if (__DEV__) {
-      return compose(
-        Reactotron.createEnhancer(),
-        applyMiddleware(sagaMiddleware)
-      );
-    }
-    return applyMiddleware(sagaMiddleware);
-  })()
+  applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(function* rootSaga() {
