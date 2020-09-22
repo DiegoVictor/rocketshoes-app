232835c0a5c641a85f95875ad345583a2f50862e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-22 18:44:23 -0300

    Move cart reducer test

--- tests/unit/store/reducers/cart.spec.js
+++ tests/unit/store/reducers/cart.spec.js
@@ -21,6 +21,15 @@ describe('Cart reducer', () => {
 
   it('REMOVE', () => {
     const product = { id: faker.random.number() };
+    const state = reducer(
+      [product],
+      removeFromCart(faker.random.number({ min: product.id + 1 }))
+    );
+    expect(state).toHaveLength(1);
+  });
+
+  it('REMOVE', () => {
+    const product = { id: faker.random.number() };
     const state = reducer([product], removeFromCart(product.id));
     expect(state).toHaveLength(0);
   });
@@ -34,5 +43,16 @@ describe('Cart reducer', () => {
       ...product,
       amount,
     });
+  });
+
+  it('UPDATE_AMOUNT_SUCCESS', () => {
+    const product = { id: faker.random.number(), amount: 1 };
+    const amount = faker.random.number({ min: 2, max: 5 });
+
+    const state = reducer(
+      [product],
+      updateAmountSuccess(faker.random.number({ min: product.id + 1 }), amount)
+    );
+    expect(state).toContainEqual(product);
   });
 });
