ca09de01dfd94c268593303285b71fee1a32abfc
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-24 00:05:03 -0300

    Mock navigation

--- tests/unit/components/Header.spec.js
+++ tests/unit/components/Header.spec.js
@@ -6,24 +6,30 @@ import Header from '~/components/Header';
 
 jest.mock('react-redux');
 
-describe('Header component', () => {
-  const navigate = jest.fn();
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
 
+describe('Header component', () => {
   it('should be able to go to Home page', () => {
     useSelector.mockImplementation(cd => cd({ cart: [] }));
-
-    const { getByTestId } = render(<Header navigate={navigate} />);
-
+    const { getByTestId } = render(<Header />);
     fireEvent.press(getByTestId('home'));
-    expect(navigate).toHaveBeenCalledWith('Home');
+    expect(mockedNavigate).toHaveBeenCalledWith('Home');
   });
 
   it('should be able to go to Cart page', () => {
     useSelector.mockImplementation(cd => cd({ cart: [] }));
 
-    const { getByTestId } = render(<Header navigate={navigate} />);
+    const { getByTestId } = render(<Header />);
 
     fireEvent.press(getByTestId('cart'));
-    expect(navigate).toHaveBeenCalledWith('Cart');
+    expect(mockedNavigate).toHaveBeenCalledWith('Cart');
   });
 });
