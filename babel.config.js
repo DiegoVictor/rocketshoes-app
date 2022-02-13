6ab3123f4b61b35a833b7e5b642cb820644158e5
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-13 10:41:11 -0300

    Move to plugins and configure reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'module:react-native-dotenv',
     [
       'babel-plugin-root-import',
       {
         rootPathSuffix: 'src',
       },
     ],
+    'react-native-reanimated/plugin',
   ],
 };
